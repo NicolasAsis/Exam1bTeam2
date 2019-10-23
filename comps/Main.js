597d62a7e4c57e74f4c9c7a7e89dc71ce78a2751
NicolasAsis <nico.asis15@gmail.com>
2019-10-23 09:54:21 -0700

    Finish Right Side

--- comps/Main.js
+++ comps/Main.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import {View,Text} from 'react-native';
+import Right from './Right';
 
 function Main(){
     return(
-        <View>
-            <Text>App</Text>
+        <View style={{
+            flexDirection:'row',
+            display:'flex',
+            height:'100%'
+        }}>
+            {/* <View style={{
+                backgroundColor:'blue',
+                height:'100%',
+                flex:2
+            }}></View> */}
+            <Right />
         </View>
     );
 }
