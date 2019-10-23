156cff4a8dc275783d32bb99a6243120dab0c2ea
Dora Tao <tsy6808@gmail.com>
2019-10-23 11:47:27 -0700

    Finish

--- comps/Left.js
+++ comps/Left.js
@@ -1,24 +1,37 @@
-import React, { Component } from 'react';
-import { Text, TextInput, View,Button,Linking} from 'react-native';
+import React, { useState } from 'react';
+import { Text, TextInput, View,Button, ImageBackground} from 'react-native';
 
 
 function Left(){
-const [value,onChangeText] = React.useState();
-const url="https://google.com";
-  return(
-    <View>
-    <Button
-    title="Set Image" onPress={() => Linking.openURL(url)}
-    />
+const [value,onChangeText] = useState();
+const[bgImg,setBgImg]= useState(false);
+var bg = null;
+
+if(bgImg === true){
+  bg =
+<ImageBackground
+source = {{uri:value}}
+style={{width:'100%',height:'100%'}}>
+</ImageBackground>
+;
+}
+return(
+  <View>
+  <Button
+  title="Set Image"
+  onPress={()=>{setBgImg(!bgImg)}}
+   />
+
+   {bg}
+
     <TextInput
     placeholder="image url"
-    onChangeText={url=> onChangeText(url)}
+    onChangeText={text=> onChangeText(text)}
     value={value}
     />
-
-
     <Text>your images url is {(value)} </Text>
-</View>
+
+  </ View>
   );
 }
 
